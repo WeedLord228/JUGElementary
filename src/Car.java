9498e665b7ba374a37de5fbc26fa6410a70ff020
Зайцев Артем Витальевич <avzajtsev@naumen.ru>
2021-09-29 13:28:14 +0500

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,35 @@
+
+public class Car
+{
+    public static final int STEAM_ENGINE_CATEGORY = 0;
+    public static final int COMBUSTION_ENGINE_CATEGORY = 1;
+    public static final int ELECTRIC_CATEGORY = 2;
+
+    private String name;
+    private int category;
+    private int wheelCount;
+
+
+    public Car(String name, int category)
+    {
+        this.name = name;
+        this.category = category;
+    }
+
+    public void methodInCar()
+    {
+    }
+
+    public void methodInCarButDifferent()
+    {
+    }
+
+    public void methodInCarButDifferentInADifferentWay()
+    {
+    }
+
+    public void methodInCarWithDifferentBehaviour()
+    {
+
+    }
+}
---
+++ src/JUGElementary.java
@@ -0,0 +1,77 @@
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class JUGElementary
+{
+    public static void main(String[] args)
+    {
+        List<Integer> integers = asList(1, 2, 3, 4, 5);
+
+    }
+
+
+    public static void methadTakingACar(Car car) {
+
+    }
+
+    public static void methodTakingACarDifferently(Car car) {
+
+    }
+
+    public static void methodTakingACarButNotWhatYouMeant(Car car) {
+
+    }
+
+    public static void methodTakingACarButNotWhatYouMeantAlsoDifferent(Car car) {
+
+    }
+
+    public static void methodDifferentFromOthersAlsoTakingTwoCars(Car car1, Car car2) {
+    }
+
+
+
+
+//     АВТОДОПОЛНЕНИЕ
+//     Alt + Enter whatever
+//     Автодополнение по первой букве car.gWC
+//     Автодополнение через точку c.gWC
+//     ПРЕДЛОЖЕНИЯ ОТ CTRL+SPACE
+//     ПРЕДЛОЖЕНИЯ ОТ CTRL + SHIFT + SPACE
+
+//      РЕФАКТОРИНГ
+//      Ctrl + [Shift] + W - для выделения блоков
+//      Alt + J, Ctrl+Alt+J  - Несколько  строк ввода
+//      Refactor -> Extract Delegate.
+
+
+//     ГЕНЕРАЦИЯ ГЕТТЕРОВ СЕТТЕРОВ
+//     Alt+insert
+//     Вместо Alt+insert по первым буквам генерировать
+
+
+//        НАВИГЦИЯ
+//      SHIFT + SHIFT
+//      Alt + Стрелки
+//      Ctrl + Alt + Стрелки
+//      Ctrl + E - Навигация по файлам
+//      Ctrl + Shift + E - Последние открытые места
+//      Ctrl + F12
+//      Ctrl + Shift + V - История клипборда
+//      Ctrl + Shift + [0,9] - Закладки
+
+//    LIVE TEMPLATES - быстрее и безопаснее.
+//    psvm
+//    psf
+//    for
+//    fori
+//    POSTFIX FUNCTIONS -
+//    [].for
+
+// Менюшки
+//    ALT + V (SHIFT + SHIFT)
+
+
+
+}
